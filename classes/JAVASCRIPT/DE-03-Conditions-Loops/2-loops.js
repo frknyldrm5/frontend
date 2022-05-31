5f1b07cab9532e3e3dbd1e48d01cb45b66d06d2b
frknyldrm5 <furkanyildirim3455.3455@gmail.com>
2022-05-31 11:18:35 +0200

    u

--- classes/JAVASCRIPT/DE-03-Conditions-Loops/2-loops.js
+++ classes/JAVASCRIPT/DE-03-Conditions-Loops/2-loops.js
@@ -4,18 +4,49 @@
 
 //************************ FOR ****************************
 
-for(let i=1; i<=10; i++){
+// for(let i=1; i<=10; i++){
 
-    console.log("MERHABA" , i );
-   }
+//     console.log("MERHABA" , i );
+//    }
    
-   //* 1 den N e kadar olan tamsayıların toplamını bulan kodu yazınız
+//    //* 1 den N e kadar olan tamsayıların toplamını bulan kodu yazınız
    
-   const n =   +prompt("N için kullanılacak sayıyı giriniz");
-   let sayi=0;
-   for (let i=1; i <= n ; i++) {
+//    const n =   +prompt("N için kullanılacak sayıyı giriniz");
+//    let sayi=0;
+//    for (let i=1; i <= n ; i++) {
    
-    sayi=sayi+i;
-    //console.log(sayi);
-   }
-   console.log(sayi);
+//     sayi=sayi+i;
+//     //console.log(sayi);
+//    }
+//    console.log(sayi);
+
+
+
+   //******************** WHILE ******************
+//Ornek1: WHİLE İÇİN 0-100 arasında bir not giriniz, aksi durumlarda error msjı verip, tekrar not isteyiniz
+
+// let nots= +prompt("WHILE ICIN 0-100 ARASi SAYI GIRINIZ")
+
+// while(nots<0 || nots>100)
+// {console.error("not 0-100 arasinda olmalidir");
+// nots= +prompt("WHILE ICIN 0-100 ARASI SAYI GIRINIZ");
+// }
+
+// console.log("girdiginiz  sayi 0-100 arasindadir");
+
+//***************DO WHILE */
+
+// do{
+//     notlar= +prompt("DO_WHILE ICIN 0-100 ARASI SAYI GIRINIZ");
+//     alert(notlar);
+
+// }while (notlar<0 || notlar>100);
+// console.log("girdiginiz not",notlar)
+
+//? ORNEK2: klavyeden Q karakteri girilene kadar not girişi yapan bir programı döngü kullanarak yazınız.
+
+// do{
+//     grade= +prompt("lutfen 2.  ornek icin 1-100 arasi sayi girin");
+// }while(grade != "q");
+// console.log(grade);
+
